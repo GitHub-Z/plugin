1774c36380171b1a00e9f98558cd202a08cf7051
GitHub-Z <monarch_zj@outlook.com>
2016-12-17 17:58:17 +0800

    zj

--- pluginurl.php
+++
@@ -1,5 +0,0 @@
-<?php  
-header('Content-Type:text/html;Charset=utf-8');  
-$arr = array('os'=>'s2l','url'=>'https://codeload.github.com/GitHub-Z/plugin/zip/master');  
-echo $_GET['jsoncallback'] . "(".json_encode($arr).")"; 
-?>
